d95be7aa6d507066e39bfb7c88a7e6bbc495ea5d
snazeczek <88104768+snazeczek@users.noreply.github.com>
2021-08-13 11:27:40 +0200

    Add files via upload

--- Funkcja pobierająca listę liczb i zwracającą drugą najmniejszą wartość (wszystkie wystąpienia).py
+++ Funkcja pobierająca listę liczb i zwracającą drugą najmniejszą wartość (wszystkie wystąpienia).py
@@ -1,10 +1,12 @@
 lista=[1, 67, 2, 4, 5, 5, 77, 2, 3, 6, 8, 3]
-lista.sort()
-listano=list(set(lista))
-listano.sort()
-l=0
-for i in range(0,len(lista)):
-    if listano[1]==lista[i]:
-        l+=1
-for x in range(1,l+1):
-    print(listano[1])
+def znajdowanie():
+    lista.sort()
+    listano=list(set(lista))
+    listano.sort()
+    l=0
+    for i in range(0,len(lista)):
+        if listano[1]==lista[i]:
+            l+=1
+    for x in range(1,l+1):
+        print(listano[1])
+znajdowanie()
